72e6578566bae72cedcfd5ae238def46341b91c2
Mark Kitz <me@markkitz.com>
2022-09-11 08:52:55 -0600

    quick change

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -168,7 +168,7 @@ export default function Header() {
             </Popover>
 
             <a href="#" className="text-base font-medium text-gray-500 hover:text-gray-900">
-              Pricing
+              Test
             </a>
             <a href="#" className="text-base font-medium text-gray-500 hover:text-gray-900">
               Docs
